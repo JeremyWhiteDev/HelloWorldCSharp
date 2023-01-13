029571f39c45099bbc6757b41ae8b19b729c3b37
Jeremy White <jcwhite414@gmail.com>
2023-01-12 19:52:18 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Howdy Y'all!!");
+Console.ReadKey(); // What happens if you run the app without this line?
+
+var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+foreach (var animal in animals)
+{
+
+    if (animal.Length > 5)
+    {
+
+    Console.WriteLine(animal);
+    }
+}
